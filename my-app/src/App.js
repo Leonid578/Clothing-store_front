04cf2495877507794ff64b5a59de0353321f27c1
Leonid578 <Leonid.Ivanov187@gmail.com>
2022-11-12 16:08:27 +0200

    upd

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,25 @@
-const App = () => {
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
   return (
-    <>
-      <h1>HI!!! welcome</h1>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
