828d8ee0880d71ae23ccfa6dbcb076e534eb026a
Leonid578 <Leonid.Ivanov187@gmail.com>
2022-12-03 09:31:45 +0200

    upd

--- src/App.js
+++ src/App.js
@@ -4,20 +4,26 @@ import Header from "./components/Header/Header";
 // import HitsOfSales from './components/hitsOfSales/HitsOfSales'
 import { Carousel } from "./components/Carousel/Carousel";
 import "./components/Carousel/Carousel.style.css";
-import Products from './components/Products/Products'
+// import Products from './components/Products/Products'
 
+import Layout from './components/Theme/Layout'
+import { ThemeProvider } from './components/providers/ThemeProvider'
 const App = () => {
   return (
     <>
-      <Header />
-      <Carousel>
+      <ThemeProvider>
+        <Layout>
+          <Header />
+          <Carousel>
         <div className="Img1"></div>
         <div className="Img2"></div>
         <div className="Img3"></div>
       </Carousel>
-      <Products/>
-      {/* <HitsOfSales /> */}
-      {/* <Footer /> */}
+          {/* <Products/> */}
+          {/* <HitsOfSales /> */}
+          {/* <Footer /> */}
+        </Layout>
+      </ThemeProvider>
     </>
   );
 };
---
+++ src/components/hooks/useTheme.jsx
@@ -0,0 +1,10 @@
+import { useContext } from 'react'
+import { ThemeContext } from '../providers/ThemeProvider'
+
+const useTheme = () => {
+	const value = useContext(ThemeContext)
+
+	return value
+}
+
+export default useTheme
