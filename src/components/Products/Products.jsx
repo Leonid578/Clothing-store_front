c1d9289203e537b563a488b2af9874bf62a826a0
Leonid578 <Leonid.Ivanov187@gmail.com>
2022-12-11 13:35:41 +0200

    upd

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -17,68 +17,63 @@
 // import ATBbag from "../../images/jpg/img00.jpg";
 // import ATBEcoBag from "../../images/jpg/img001.jpg";
 // import BMWbag from "../../images/jpg/img002.jpg";
-import '../Products/Products.css';
+
+import "../Products/Products.css";
 const ProductsCard = () => {
-    return (
-        <section class="cards__content">
-            <ul class="cards__ul">
-                <li class="card">
-                    <div class="product__img">
-                        {/* <img src="" alt=""> */}
-                    </div>
-                    <div class="card__inf">
-                        <div class="product__name">
-                            <p>фол</p>
-                            <span></span>
-                        </div>
-                        <div class="product__cost">
-                            <p>Польное название</p>
-                            <span>цена</span>
-                        </div>
-                        <div class="product__btn">
-                            <button className="btn__go">Перейти</button>
-                        </div>
-                    </div>
-                </li>
-                <li class="card">
-                <div class="product__img">
-                    {/* <img src="" alt=""> */}
-                </div>
-                <div class="card__inf">
-                    <div class="product__name">
-                        <p>кап</p>
-                        <span></span>
-                    </div>
-                    <div class="product__cost">
-                        <p>Польное название</p>
-                        <span>цена</span>
-                    </div>
-                    <div class="product__btn">
-                        <button className="btn__go">Перейти</button>
-                    </div>
-                </div>
-                </li>
-                <li class="card">
-                <div class="product__img">
-                    {/* <img src="" alt=""> */}
-                </div>
-                <div class="card__inf">
-                    <div class="product__name">
-                        <p>геп</p>
-                        <span></span>
-                    </div>
-                    <div class="product__cost">
-                        <p>Польное название</p>
-                        <span>цена</span>
-                    </div>
-                    <div class="product__btn">
-                    <button className="btn__go">Перейти</button>
-                    </div>
-                </div>
-                </li>
-            </ul>
-        </section>
-    );
+  return (
+    <section class="cards__content">
+      <ul class="cards__ul">
+        <li class="card">
+          <div class="product__img">{/* <img src="" alt=""> */}</div>
+          <div class="card__inf">
+            <div class="product__name">
+              <p>фол</p>
+              <span></span>
+            </div>
+            <div class="product__cost">
+              <p>Польное название</p>
+              <span>цена</span>
+            </div>
+            <div class="product__btn">
+              <button className="btn__go">Перейти</button>
+            </div>
+          </div>
+        </li>
+        <li class="card">
+          <div class="product__img">{/* <img src="" alt=""> */}</div>
+          <div class="card__inf">
+            <div class="product__name">
+              <p>кап</p>
+              <span></span>
+            </div>
+            <div class="product__cost">
+              <p>Польное название</p>
+              <span>цена</span>
+            </div>
+            <div class="product__btn">
+              <button className="btn__go">Перейти</button>
+            </div>
+          </div>
+        </li>
+        <li class="card">
+          <div class="product__img">{/* <img src="" alt=""> */}</div>
+          <div class="card__inf">
+            <div class="product__name">
+              <p>геп</p>
+              <span></span>
+            </div>
+            <div class="product__cost">
+              <p>Польное название</p>
+              <span>цена</span>
+            </div>
+            <div class="product__btn">
+              <button className="btn__go">Перейти</button>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </section>
+  );
 };
 
 export default ProductsCard;
